4b2dd9cc3bb4e69d72ad8e7d0af6f259b7c8dc30
Maximilian Gruber <m.gruber@mailbox.org>
2019-09-08 10:11:23 +0200

    Change minor things with copy_script

--- copy_script.py
+++ copy_script.py
@@ -1,18 +1,53 @@
-
 import pyudev
 import subprocess
+import os
+import shutil
+import datetime
+import sys
 
 def main():
-    context = pyudev.Context()
-    monitor = pyudev.Monitor.from_netlink(context)
-    monitor.filter_by(subsystem='usb')
-    monitor.start()
 
-    for device in iter(monitor.poll, None):
-        # I can add more logic here, to run different scripts for different devices.
-        print(device)
+    # path definitions
+    mount_label = "flashdrive"
+    mount_point = os.path.join("/media", mount_label)
 
-        #subprocess.call(['/home/foo/foobar.sh', '--foo', '--bar'])
+    orig_logfolder = os.getcwd()
+    orig_logfile   = os.path.join(orig_logfolder, "logfile.csv")
+
+    dest_logfolder = os.path.join(mount_point, "logs")
+    dest_logfile   = os.path.join(dest_logfolder, "log_{DATE}.csv")
+
+    try: 
+        # monitor kernel for block-device messages
+        context = pyudev.Context()
+        monitor = pyudev.Monitor.from_netlink(context)
+        monitor.filter_by(subsystem='block')
+        monitor.start()
+
+        for device in iter(monitor.poll, None):
+            if 'ID_FS_TYPE' in device:
+                print('{0} partition {1} at {2}'.format(device.action, device.get('ID_FS_LABEL'), device.device_node))        
+
+                if device.action == "add":
+
+                    # mount the block-device
+                    subprocess.call(['pmount', device.device_node, mount_label])
+
+                    # create folder if necessary
+                    if not os.path.exists(dest_logfolder):
+                        os.mkdir(dest_logfolder)
+
+                    # copy file
+                    date = datetime.datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
+                    shutil.copy(orig_logfile, dest_logfile.format(DATE=date))
+
+                    # unmount device
+                    subprocess.call(['pumount', mount_label])
+
+    except KeyboardInterrupt:
+        print("Exiting. May leave mounted drives behind.")
+        sys.exit(0)
+
 
 if __name__ == '__main__':
     main()
