6f732b538c82df1c3423fa441fe811f66a599ef9
Maximilian Gruber <m.gruber@mailbox.org>
2019-09-09 22:43:04 +0200

    Changed logic for more robust blink detection
    - Also added detection for steady warning light

--- logger.py
+++ logger.py
@@ -2,15 +2,19 @@ import sys
 import asyncio
 import time
 import datetime
+import os
 
 
 class Logger():
 
     def __init__(self, BCM_PIN=4):
+        self.logfile = os.path.join(os.getcwd(), "logfile.csv")
+
         self.bounce_time = 3  # seconds
 
         self.last_rising_edge = 0.0
         self.last_falling_edge = 0.0
+        self.last_edge = 0.0
 
         # RPi settings
         self.PIN = BCM_PIN
@@ -19,6 +23,8 @@ class Logger():
         self.cmd_enable_read    = "gpio -g mode {PIN} down".format(PIN=self.PIN)  # input/output/up/down/tri
         self.cmd_detect_rising  = "gpio -g wfi {PIN} rising".format(PIN=self.PIN)  # rising/falling/both
         self.cmd_detect_falling = "gpio -g wfi {PIN} falling".format(PIN=self.PIN)
+        self.cmd_detect_both    = "gpio -g wfi {PIN} both".format(PIN=self.PIN)
+        self.cmd_current_state  = "gpio -g read {PIN}".format(PIN=self.PIN)
         
 
     async def on_rising_edge(self, timestamp):
@@ -26,19 +32,38 @@ class Logger():
         self.last_rising_edge = timestamp
 
         # only log, if the last falling edge was more than bounce_time seconds in the past
-        if timestamp - tmp > self.bounce_time:
+        # also check, that the last falling edge is also at least bounce_time seconds in the past
+        if (timestamp > tmp + self.bounce_time) and (timestamp > self.last_falling_edge + self.bounce_time):
             self.write_to_logfile("AN", timestamp)
 
 
     async def on_falling_edge(self, timestamp):
+        tmp = self.last_rising_edge
         self.last_falling_edge = timestamp
 
         # only log, if after a wait of bounce_time the last falling edge wasn't updated
         await asyncio.sleep(self.bounce_time)
 
         # self.last_falling_edge could have been altered by another call of on_falling_edge in the meantime
-        if self.last_falling_edge - timestamp == 0:
+        # also check that was no rising-edge in the meantime
+        if (timestamp == self.last_falling_edge) and (tmp == self.last_rising_edge):
             self.write_to_logfile("AUS", timestamp)
+
+    async def on_edge(self, timestamp):
+        tmp = self.last_edge
+        self.last_edge = timestamp
+
+        # only log, if after a wait of bounce_time the last falling edge wasn't updated
+        await asyncio.sleep(self.bounce_time)
+
+        # self.last_falling_edge could have been altered by another call of on_falling_edge in the meantime
+        # also check that was no rising-edge in the meantime
+        if (timestamp == self.last_edge):
+            current_state = int(await self.run(self.cmd_current_state))
+            if current_state == 0:
+                self.write_to_logfile("AUS", timestamp)
+            else:
+                self.write_to_logfile("AN", timestamp)
 
 
     def write_to_logfile(self, text, timestamp):
@@ -50,7 +75,7 @@ class Logger():
         logtext = "{TS}, {TIME}, {TXT}\n".format(TXT=text, TIME=timestring, TS=timestamp)
 
         print(logtext)
-        f = open("logfile.csv", "a")
+        f = open(self.logfile, "a")
         f.write(logtext)
         f.close()
 
@@ -69,7 +94,15 @@ class Logger():
         future_timestamp.set_result(time.time())
 
 
-    async def detection_cycle(self, loop, start_next_cycle):
+    async def detect_edge(self, PIN, future_timestamp):
+        await self.run(self.cmd_detect_falling)
+
+        # return the value asynchronously
+        future_timestamp.set_result(time.time())
+
+
+    # better suited for blinking warning light
+    async def detection_cycle_blink(self, loop, start_next_cycle):
         # create elements to store the future result of edge-detection
         rising_edge      = loop.create_future()
         falling_edge     = loop.create_future()
@@ -87,12 +120,30 @@ class Logger():
         await loop.create_task(self.on_falling_edge(await falling_edge))
 
 
+    # better suited for continous warning light
+    async def detection_cycle_steady(self, loop, start_next_cycle):
+        # create elements to store the future result of edge-detection
+        edge      = loop.create_future()
+        
+        # create task to detect edge and do something with result
+        loop.create_task(self.detect_edge(self.PIN, edge))
+       
+        # start the next cycle as soon as a rising edge is detected
+        await edge
+        start_next_cycle.set_result(True)
+        
+        # logging
+        await loop.create_task(self.on_edge(await edge))
+
+
     async def run(self, cmd):
         proc = await asyncio.create_subprocess_shell(cmd,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE)
 
         stdout, stderr = await proc.communicate()
+        
+        return stdout
 
         #print(f'[{cmd!r} exited with {proc.returncode}]')
         #if stdout:
@@ -112,7 +163,8 @@ async def main():
 
     while True:
             start_next_cycle = loop.create_future()
-            loop.create_task(logger.detection_cycle(loop, start_next_cycle))
+            loop.create_task(logger.detection_cycle_blink(loop, start_next_cycle))
+            #loop.create_task(logger.detection_cycle_steady(loop, start_next_cycle))
 
             await start_next_cycle
 
