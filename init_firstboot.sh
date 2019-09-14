7bdb36bd48577386c058ad4367f17e1510ae87d6
Maximilian Gruber <m.gruber@mailbox.org>
2019-09-14 17:29:04 +0200

    remove bugs in init_firstboot.sh

--- init_firstboot.sh
+++ init_firstboot.sh
@@ -2,31 +2,39 @@
 # one-time-run firstboot.service on first start of the RPi. 
 # It allows to setup the RPi without keyboard/ssh-access.
 
+#### init ####
+
+# new hostname of the RPi
+NEW_NAME="Logger_01"
+
 # define the mount points of the boot and rootfs partitions on the RPi-SD-card
-export ROOTFS=/media/$USER/rootfs
-export BOOTFS=/media/$USER/boot
+ROOTFS="/media/$USER/rootfs"
+BOOTFS="/media/$USER/boot"
 
 # directories of interest
-export PI_HOME=$ROOTFS/home/pi
-export LOGGER=$PI_HOME/logger
+PI_HOME="$ROOTFS/home/pi"
+LOGGER="$PI_HOME/logger"
+
+
+#### main ####
 
 # clone into git repo
 cd $PI_HOME
-git clone https://github.com/mgrub/logger
+if [ -d "$LOGGER" ]; then
+  cd $LOGGER
+  git pull origin master
+else
+  git clone https://github.com/mgrub/logger
+fi
 
 # copy the script itself and the unit-file
-sudo cp $LOGGER/firstrun.sh $BOOTFS
-sudo cp $LOGGER/firstrun.service $ROOTFS/etc/systemd/system/
+cp $LOGGER/firstboot.sh $BOOTFS
+sudo cp $LOGGER/firstboot.service $ROOTFS/etc/systemd/system/
 
 # enable systemd-services
 cd $ROOTFS/etc/systemd/system/multi-user.target.wants
-sudo ln -sf /etc/systemd/system/firstboot.service firstboot.service
-
-# does the same as: sudo systemctl enable firstboot.service
+sudo ln -sf /etc/systemd/system/firstboot.service firstboot.service  # (does the same as: sudo systemctl enable firstboot.service)
 
 # change hostname
-NEW_NAME="Logger_01"
-echo $NEW_NAME > $ROOTFS/etc/hostname
-sed -i "s/raspberrypi/$NEW_NAME/g" $ROOTFS/etc/hosts
-hostname $NEW_NAME
-
+echo $NEW_NAME | sudo tee $ROOTFS/etc/hostname
+sudo sed -i "s/raspberrypi/$NEW_NAME/g" $ROOTFS/etc/hosts
